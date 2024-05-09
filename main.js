97cdce1f53e59ad8d1b8e447affd22cc8c09bd9d
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2024-05-09 15:25:51 +0300

    fix

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const videoFileInput = document.getElementById('videoFileInput');
 const videoPlayer = document.getElementById('videoPlayer');
+const playBackRate = document.querySelector(".playbackrate")
 
 videoFileInput.addEventListener('change', function() {
     const file = this.files[0];
     const url = URL.createObjectURL(file);
     videoPlayer.src = url;
+});
+
+playBackRate.addEventListener('change', function() {
+   videoPlayer.playbackRate = +this.value
 });
